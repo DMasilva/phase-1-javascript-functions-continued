570b8d9401663fb1792b7f12f96174fa119c826b
David <david.owuor@student.moringaschool.com>
2022-11-22 23:02:36 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+
+function saturdayFun(defaultActivity = "roller-skate"){
+    return `This Saturday, I want to ${defaultActivity}!`
+}
+function mondayWork(mondayActivity='go to the office'){
+        return `This Monday, I will ${mondayActivity}.`
+}
+
+function wrapAdjective(signature = "||"){
+    return function(adjective = "a dedicated programmer "){
+        return `You are ${signature}${adjective}${signature}!`
+    }
+}
+
